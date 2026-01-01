ef179251c04860aa52db2f1b7bffe7eb72e9152d
Habteweld Mazengia Eskezia <habteweldmaz999@gmail.com>
2026-01-01 10:07:34 +0300

    Add files via upload

---
+++ login_sign_up.js
@@ -0,0 +1,11 @@
+// Show Sign Up Form
+function showSignUp() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("register").style.display = "block";
+}
+
+// Show Login Form
+function showSignIn() {
+  document.getElementById("register").style.display = "none";
+  document.getElementById("login").style.display = "block";
+}
